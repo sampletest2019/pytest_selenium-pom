ddc6a7525196269780d05bb1ac6a06a1f8b97bf3
QA Real <qareal@outlook.com>
2020-06-05 10:23:12 -0400

    conftest updated. Exception added for unsupported OS.

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,8 +10,13 @@ def browser():
         browser = webdriver.Chrome("../resources/chromedriver_83.exe")
     elif 'Mac' in platform.platform():
         browser = webdriver.Chrome("../resources/chromedriver_mac_83.exe")
+    elif "Linux" in platform.platform():
+        browser = webdriver.Chrome("../resources/chromedriver_linux_83")
+    else:
+        raise Exception("chromedriver is not configured for your Operation System! "
+                        "Your Operating System is: {}". format(platform.platform()))
 
-    # wait 10 seconds till the website will open
+    # wait 10 seconds to pull the DOM
     browser.implicitly_wait(10)
     # maximize browser window to full screen
     browser.maximize_window()
